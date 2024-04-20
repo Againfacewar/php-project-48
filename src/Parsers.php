29a2cd730f39bc123958613aae8aa22d48ef5ce2
Ucsus <ucsus@exbo.ru>
2024-04-20 20:03:27 +0300

    done?

--- src/Parsers.php
+++ src/Parsers.php
@@ -13,7 +13,7 @@ function parser(string $file1, string $file2): array
 {
     $filePath1 = realpath($file1);
     $filePath2 = realpath($file2);
-    if (!$filePath1 || !$filePath2) {
+    if (!file_exists($filePath1) || !file_exists($filePath2)) {
         throw new \Exception('One or both of the transferred files were not found.
          Make sure that the specified paths are correct!');
     }
@@ -23,8 +23,8 @@ function parser(string $file1, string $file2): array
         $encodedFile = file_get_contents($path);
 
         return match ($ext) {
-            'yaml', 'yml' => Yaml::parse($encodedFile),
-            'json' => json_decode($encodedFile, true),
+            'yaml', 'yml' => Yaml::parse((string) $encodedFile),
+            'json' => json_decode((string) $encodedFile, true),
             default => throw new \Exception('Данный формат файла не поддерживается!'),
         };
     });
