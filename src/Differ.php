51ee0197f414b3f066c4ca1eb77334e041ae8b48
Ucsus <ucsus@exbo.ru>
2024-04-20 19:35:10 +0300

    small fix

--- src/Differ.php
+++ src/Differ.php
@@ -23,30 +23,34 @@ function compareFiles(array $firstFile, array $secondFile, int $depth, bool $isN
     $keys = [...array_unique(array_merge(array_keys($firstFile), array_keys($secondFile)))];
     $sortedKeys = sort($keys, fn($left, $right) => strcmp($left, $right));
 
-    return reduce_left($sortedKeys, function ($item, $key, $map, $acc) use ($firstFile, $secondFile, $depth, $isNested) {
+    return reduce_left(
+        $sortedKeys,
+        function ($item, $key, $map, $acc) use ($firstFile, $secondFile, $depth, $isNested) {
 
-        if (array_key_exists($item, $firstFile) && array_key_exists($item, $secondFile)) {
-            if (is_array($firstFile[$item]) && is_array($secondFile[$item])) {
-                $result =
+            if (array_key_exists($item, $firstFile) && array_key_exists($item, $secondFile)) {
+                if (is_array($firstFile[$item]) && is_array($secondFile[$item])) {
+                    $result =
                     ['type' => 'internal', "key" => $item,
                         'children' => compareFiles($firstFile[$item], $secondFile[$item], $depth + 1, $isNested)] ;
-                if (!empty($result)) {
-                    $acc[] = $result;
-                }
-            } elseif ($firstFile[$item] === $secondFile[$item]) {
-                $acc[] = ['type' => 'unchanged', 'key' => $item, 'value' => $firstFile[$item]];
-            } else {
-                $acc[] =
+                    if (!empty($result)) {
+                        $acc[] = $result;
+                    }
+                } elseif ($firstFile[$item] === $secondFile[$item]) {
+                    $acc[] = ['type' => 'unchanged', 'key' => $item, 'value' => $firstFile[$item]];
+                } else {
+                    $acc[] =
                     ['type' => 'updated', 'key' => $item,
                         'value' => ['first' => $firstFile[$item], 'second' => $secondFile[$item]]];
+                }
+            } elseif (array_key_exists($item, $firstFile)) {
+                $acc[] = ['type' => 'removed', 'key' => $item, 'value' => $firstFile[$item]];
+            } elseif (array_key_exists($item, $secondFile)) {
+                $acc[] = ['type' => 'added', 'key' => $item, 'value' => $secondFile[$item]];
             }
-        } elseif (array_key_exists($item, $firstFile)) {
-            $acc[] = ['type' => 'removed', 'key' => $item, 'value' => $firstFile[$item]];
-        } elseif (array_key_exists($item, $secondFile)) {
-            $acc[] = ['type' => 'added', 'key' => $item, 'value' => $secondFile[$item]];
-        }
-        return $acc;
-    }, []);
+            return $acc;
+        },
+        []
+    );
 }
 
 function isNested(array $map): bool
