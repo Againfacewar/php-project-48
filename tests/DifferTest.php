50835c6d4061d6e1a47327d7190965f477ba91e3
Ucsus <ucsus@exbo.ru>
2024-04-22 12:39:48 +0300

    code review

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -3,70 +3,46 @@
 namespace Hexlet\Code\Tests;
 
 use Exception;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 use function Differ\Differ\genDiff;
 
 class DifferTest extends TestCase
 {
-    private string $jsonFile1;
-    private string $jsonFile2;
-    private string $yamlFile1;
-    private string $yamlFile2;
     private string $path = __DIR__ . "/fixtures/";
-    private string $exceptedStylish;
-    private string $exceptedPlain;
-    private string $exceptedJson;
+
     private function getRealPath($name): string
     {
         return $this->path . $name;
     }
-    public function setUp(): void
+
+    public static function dataProvider(): array
     {
-        $this->jsonFile1 = $this->getRealPath('file1.json');
-        $this->jsonFile2 = $this->getRealPath('file2.json');
-        $this->yamlFile1 = $this->getRealPath('file1.yml');
-        $this->yamlFile2 = $this->getRealPath('file2.yml');
-        $stylishData = file_get_contents($this->getRealPath('stylishExcepted.txt'));
-        $this->exceptedStylish = explode("\n\n\n", $stylishData)[0];
-        $plainData = file_get_contents($this->getRealPath('plainExcepted.txt'));
-        $this->exceptedPlain = explode("\n\n\n", $plainData)[0];
-        $plainData = file_get_contents($this->getRealPath('jsonExcepted.txt'));
-        $this->exceptedJson = explode("\n\n\n", $plainData)[0];
+        return [
+            ["stylishExcepted.txt", "file1.json", "file2.json"],
+            ["stylishExcepted.txt", "file1.yml", "file2.yml"],
+            ["plainExcepted.txt", "file1.json", "file2.json", "plain"],
+            ["plainExcepted.txt", "file1.yml", "file2.yml", "plain"],
+            ["jsonExcepted.txt", "file1.json", "file2.json", "json"],
+            ["jsonExcepted.txt", "file1.yml", "file2.yml", "json"],
+        ];
     }
 
     /**
      * @throws Exception
      */
-
-    public function testStylishFormat(): void
+    #[DataProvider('dataProvider')]
+    public function testMainFlow(string $excepted, string $file1, string $file2, string $format = 'stylish'): void
     {
-        $this->assertEquals($this->exceptedStylish, genDiff($this->jsonFile1, $this->jsonFile2));
-        $this->assertEquals($this->exceptedStylish, genDiff($this->yamlFile1, $this->yamlFile2));
+        $data = file_get_contents($this->getRealPath($excepted));
+        $exceptedData = explode("\n\n\n", $data)[0];
+        $this->assertEquals($exceptedData, genDiff($this->getRealPath($file1), $this->getRealPath($file2), $format));
     }
-
-    /**
-     * @throws Exception
-     */
-    public function testPlainFormat(): void
-    {
-        $this->assertEquals($this->exceptedPlain, genDiff($this->jsonFile1, $this->jsonFile2, 'plain'));
-        $this->assertEquals($this->exceptedPlain, genDiff($this->yamlFile1, $this->yamlFile2, 'plain'));
-    }
-
-    /**
-     * @throws Exception
-     */
-    public function testJsonFormat(): void
-    {
-        $this->assertEquals($this->exceptedJson, genDiff($this->jsonFile1, $this->jsonFile2, 'json'));
-        $this->assertEquals($this->exceptedJson, genDiff($this->yamlFile1, $this->yamlFile2, 'json'));
-    }
-
     public function testBorderlineCases(): void
     {
         $this->expectException(Exception::class);
-        genDiff($this->jsonFile1, '/dir' . $this->jsonFile2);
-        genDiff($this->yamlFile1, $this->yamlFile2, 'otherFormat');
+        genDiff($this->getRealPath("file1.json"), '/dir' . $this->getRealPath("file2.json"));
+        genDiff($this->getRealPath("file1.json"), $this->getRealPath("file2.json"), 'otherFormat');
     }
 }
